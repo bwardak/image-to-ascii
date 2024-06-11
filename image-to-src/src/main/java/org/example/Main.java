ee470ad9627a7764b4d2a37169dd6d98d039f3f2
bwardak <baheer.wardak06@gmail.com>
2024-06-11 20:22:43 +0100

    added formula to calculate brightness

--- image-to-src/src/main/java/org/example/Main.java
+++ image-to-src/src/main/java/org/example/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         FileWriter writer = new FileWriter("C:\\Users\\bahee\\Documents\\Coding\\image-to-ascii\\src\\main\\ascii.txt");
 
-        File image = new File("C:\\Users\\bahee\\Documents\\daki.jpg");
+        File image = new File("C:\\Users\\bahee\\Documents\\jaegerrr.jpg");
 
         BufferedImage img = ImageIO.read(image);
 
@@ -26,6 +26,10 @@ public class Main {
                 int red = color.getRed();
                 int blue = color.getBlue();
                 int green = color.getGreen();
+
+                double brightness = 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+
+                System.out.println(brightness);
             }
         }
     }
